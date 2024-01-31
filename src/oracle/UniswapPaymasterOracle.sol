84faabe0e5a5b5fef6b0c3ffc702a6d45a246a12
jhw <justin84602@gmail.com>
2024-01-31 15:08:57 +0800

    fix: get uniswap oracle price

--- src/oracle/UniswapPaymasterOracle.sol
+++ src/oracle/UniswapPaymasterOracle.sol
@@ -27,10 +27,14 @@ contract UniswapPaymasterOracle is IPaymasterOracle {
     /// @inheritdoc IPaymasterOracle
     function getPrice(address from, uint128 amount) public view override returns (uint256) {
         address pool = pools[from];
+        address token0 = IUniswapV3Pool(pool).token0();
+        address token1 = IUniswapV3Pool(pool).token1();
+        address erc20Token = nativeToken == token0 ? token1 : token0;
+
         (, int24 tick,,,,,) = IUniswapV3Pool(pool).slot0();
 
         uint256 feeAmount =
-            OracleLibrary.getQuoteAtTick(tick, amount, IUniswapV3Pool(pool).token0(), IUniswapV3Pool(pool).token1());
+            OracleLibrary.getQuoteAtTick(tick, amount, nativeToken, erc20Token);
 
         return feeAmount;
     }
